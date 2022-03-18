9cdd3778b6d9f04d114297180a7a6f5157377b56
VekrisTizara <94655538+VekrisTizara@users.noreply.github.com>
2022-03-18 21:33:34 +0100

    add cat.png and possibility to download it (/cat)

--- homework_03/fastapi_app.py
+++ homework_03/fastapi_app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -6,5 +7,7 @@ app = FastAPI()
 def ping():
     return {"message":"pong"}
 
-
-
+@app.get("/cat", responses={200: {"content" : {"image/png" : {}}}})
+def image_endpoint():
+    file_path = "cat.png"
+    return FileResponse(file_path, media_type="image/png", filename="cat.png")
