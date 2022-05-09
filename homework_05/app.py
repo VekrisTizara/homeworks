f7e2af324dff26315cfa12c112b788a4fe5b2ee0
VekrisTizara <94655538+VekrisTizara@users.noreply.github.com>
2022-05-10 01:11:00 +0200

    do homework_05

--- homework_05/app.py
+++ homework_05/app.py
@@ -6,18 +6,19 @@
 создайте index views /
 добавьте страницу /about/, добавьте туда текст
 создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
-в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их (!!!)
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
 from flask import Flask
+from flask import Blueprint, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!1</>"
+    return render_template("home.html", text="Hello, World!")
 
 @app.route("/about/")
 def about():
-    return "This page had been made with Python"
+    return render_template("./about.html", text="This page had been made with Python")
